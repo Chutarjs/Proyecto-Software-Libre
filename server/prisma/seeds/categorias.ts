ca51d62e38ded0ad996fa027106b655aae661243
Jesus Rojas <jesus.rojas@bis.co.cr>
2024-02-27 23:03:33 -0600

    Categorias seeder

---
+++ server/prisma/seeds/categorias.ts
@@ -0,0 +1,18 @@
+
+export const categorias = [
+  //Usuario 1 
+  {
+    id: 1,
+    nombre: "Graficas",
+  },
+  //Usuario 2 
+  {
+    id: 2,
+    nombre: "Tarjeta Madre",
+  },
+  //Usuario 3 
+  {
+    id: 3,
+    nombre: "CPU",
+  },
+];
