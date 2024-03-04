8edfe8e2bcf3d02081b1d413f177949c5b69301a
Maria <mediaz@est.utn.ac.cr>
2024-03-04 00:16:10 -0600

    nn

--- client/StarTech/src/app/producto/producto-routing.module.ts
+++ client/StarTech/src/app/producto/producto-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoIndexComponent } from './producto-index/producto-index.component';
+import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
+import { ProductoAllComponent } from './producto-all/producto-all.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'producto', 
+    component: ProductoIndexComponent
+  },
+  {
+    path: 'producto-table',
+    component: ProductoAllComponent
+  },
+  {
+    path:'producto/:id',component: ProductoDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/StarTech/src/app/producto/producto.module.ts
+++ client/StarTech/src/app/producto/producto.module.ts
@@ -2,13 +2,48 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductoRoutingModule } from './producto-routing.module';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';  
+import { MatMenuModule } from '@angular/material/menu';
+import {MatDialogModule} from "@angular/material/dialog";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button';
+import { ProductoIndexComponent } from './producto-index/producto-index.component';
+import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
+import { ProductoAllComponent } from './producto-all/producto-all.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductoIndexComponent,
+    ProductoDetailComponent,
+    ProductoAllComponent
+  ],
   imports: [
     CommonModule,
-    ProductoRoutingModule
+    ProductoRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatChipsModule,
   ]
 })
 export class ProductoModule { }
