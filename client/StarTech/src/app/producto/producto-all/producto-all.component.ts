333f40384295b54de359961eb684071d7a351c27
Maria <mediaz@est.utn.ac.cr>
2024-03-06 23:10:51 -0600

    holi

--- client/StarTech/src/app/producto/producto-all/producto-all.component.ts
+++ client/StarTech/src/app/producto/producto-all/producto-all.component.ts
@@ -1,8 +1,12 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ProductoAllDataSource, ProductoAllItem } from './producto-all-datasource';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from '../../share/generic.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ProductoDiagComponent } from '../producto-diag/producto-diag.component';
 
 @Component({
   selector: 'app-producto-all',
@@ -10,17 +14,51 @@ import { ProductoAllDataSource, ProductoAllItem } from './producto-all-datasourc
   styleUrl: './producto-all.component.css'
 })
 export class ProductoAllComponent implements AfterViewInit {
+  datos: any
+  destroy$: Subject<boolean>=new Subject<boolean>()
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<ProductoAllItem>;
-  dataSource = new ProductoAllDataSource();
+  // @ViewChild(MatTable) table!: MatTable<ProductoAllItem>;
+  dataSource = new MatTableDataSource<any>();
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['nombre', 'costoUnitario','acciones'];
+
+  constructor(private gService:GenericService,
+    private dialog:MatDialog){
+
+  }
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    this.listaProductos()
+  }
+
+  listaProductos(){
+    //localhost:3000/videojuego
+    this.gService.list('producto/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data)=>{
+        console.log(data)
+        this.datos=data
+        this.dataSource = new MatTableDataSource(this.datos)
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        
+      })
+  }
+  detalleProducto(id:number){
+    const dialogConfig=new MatDialogConfig()
+    dialogConfig.disableClose=false;
+    dialogConfig.width='50%'
+    dialogConfig.data={
+      id:id
+    }
+    this.dialog.open(ProductoDiagComponent,dialogConfig)
+    
+  }
+  ngOnDestroy(){
+    this.destroy$.next(true)
+    this.destroy$.unsubscribe()
   }
 }
--- client/StarTech/src/app/producto/producto-index/producto-index.component.ts
+++ client/StarTech/src/app/producto/producto-index/producto-index.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from '../../share/generic.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-index',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class ProductoIndexComponent {
 
+  //idUsuario=2
+  datos: any //Respuesta del API
+  destroy$:Subject<boolean>=new Subject<boolean>();
+
+  constructor(
+    private gService: GenericService,
+    private router: Router
+    )
+    {
+    this.listaProductos()
+  }
+  //Listar todos los videojuegos llamando al API
+  listaProductos(){
+    //localhost:3000/videojuego
+    this.gService.list('producto/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data)=>{
+        console.log(data)
+        this.datos=data
+      })
+  }
+  detalleProducto(id:number){
+    this.router.navigate(['/producto',id])
+  }
+
+  ngOnDestroy(){
+    this.destroy$.next(true)
+    this.destroy$.unsubscribe()
+  }
 }
